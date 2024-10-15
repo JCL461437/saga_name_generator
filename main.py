0f1c10fa6cd72d1720b5563eb08bb2b3f817ab0b
Jan Lehigh <janlehigh461437@gmail.com>
2024-10-15 09:02:07 -0600

    Add fix in methods for if user input is equal to none

--- main.py
+++ main.py
@@ -1,32 +1,58 @@
 from fictional_names import name_generator
 
-def slavic_names():
-  print("Slavic:")
-  print(name_generator.generate_name(style='slavic'))
+def slavic_names(number):
+  if number != None:
+    for num in number: 
+      print(name_generator.generate_name(gender='male', style='slavic'))
+  else:
+    print("Skipped")
 
-def viking_names():
-  print("Viking:")
-  print(name_generator.generate_name(style='viking'))
+def viking_names(number):
+  if number != None:  
+    for num in number: 
+      print(name_generator.generate_name(gender='male', style='viking'))
+  else:
+    print("Skipped")
+  
+def elf_names(number):
+  if number != None:
+    for num in number: 
+      print(name_generator.generate_name(gender='male', style='elven', library=True))
+  else:
+    print("Skipped")
 
-def elf_names():
-  print("Elf:")
-  print(name_generator.generate_name(style='elven', library=True))
+def orc_names(number):
+  if number != None:
+    for num in number: 
+      print(name_generator.generate_name(gender='male', style='orc', library=True))
+  else:
+    print("Skipped")
 
-def orc_names():
-  print("Orc:")
-  print(name_generator.generate_name(style='orc', library=True))
+def dwarf_names(number):
+  if number != None:
+    for num in number: 
+      print(name_generator.generate_name(gender='male', style='dwarven', library=False))
+  else:
+    print("Skipped")
 
-def dwarf_names():
-  print("Dwarf:")
-  print(name_generator.generate_name(gender='male', style='dwarven', library=False))
+def generate_names(number, gender, style, library):
+  for num in number: 
+    print(name_generator.generate_name(gender=gender, style=style, library=True))
+  else:
+    print("Skipped")
 
 print("")
-slavic_names()
+print("How many slavic names do you want to generate?")
+slavic_names(int(input()))
 print("")
-viking_names()
+print("How many viking names do you want to generate?")
+viking_names(int(input()))
 print("")
-elf_names()
+print("How many elf names do you want to generate?")
+elf_names(int(input()))
 print("")
-orc_names()
+print("How many orc names do you want to generate?")
+orc_names(int(input()))
 print("")
-dwarf_names()
+print("How many dwarf names do you want to generate?")
+dwarf_names(int(input()))
