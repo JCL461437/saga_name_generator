d0dad969692832e6164a23242ee42067a91a5483
Jan Lehigh <janlehigh461437@gmail.com>
2024-12-04 14:56:47 -0700

    Remove superfluous functions for name generation and refactored so that all functionality is contained in one function

--- main.py
+++ main.py
@@ -1,34 +1,9 @@
 from fictional_names import name_generator
 
-def slavic_names(number):
-  if number != 0:
-    for num in number: 
-      names = name_generator.generate_name(gender='male', style='slavic')
-
-def viking_names(number):
-  if number != 0:  
-    for num in number: 
-      names = name_generator.generate_name(gender='male', style='viking')
-
-### Fantasy Names? ###
-
-def elf_names(number):
-  if number != 0:
-    for num in number: 
-      names = name_generator.generate_name(gender='male', style='elven', library=True)
-
-def orc_names(number):
-  if number != 0:
-    for num in number: 
-      names = name_generator.generate_name(gender='male', style='orc', library=True)
-
-def dwarf_names(number):
-  if number != 0:
-    for num in number: 
-      names = name_generator.generate_name(gender='male', style='dwarven', library=False)
-
 ### General Names? ###
-def generate_names(number, gender, style, library):
-  for num in number: 
-    names = name_generator.generate_name(gender=gender, style=style, library=True)
+def generate_names(gender, style, library):
+  name = name_generator.generate_name(gender=gender, style=style, library=True)
+  return name
+
+print(generate_names('male','orc',True))
 
