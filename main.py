2a0715586c1677aefdb479922987f06f39610954
Jan Lehigh <janlehigh461437@gmail.com>
2024-12-04 15:10:10 -0700

    Add os, requests, and flask modules/libraries to be imported and necessary json formatting and api endpoint

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from fictional_names import name_generator
+import os
+import requests
+import flask import Flask, jsonify, request 
+import flask_cors import CORS
 
-### General Names? ###
+# Initialize the Flask app
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+### Function for Name Generation and API Endpoint ###
+@app.route('/api/v1/generate_names?gender=<gender>?style=<style>?library=<library>')
 def generate_names(gender, style, library):
   name = name_generator.generate_name(gender=gender, style=style, library=True)
-  return name
+  return jsonify({
+    "data": {
+      "type": "{} {} name".format(style, gender),
+      "attributes": {
+        "name": name,
+        "gender": gender,
+        "style": style
+      }
+    }
+  })
 
-print(generate_names('male','orc',True))
+# Run the Flask app
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    host = os.environ.get('HOST', 'localhost')
+    socketio.run(app, host=host, port=port, debug=True)
 
